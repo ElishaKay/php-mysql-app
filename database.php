877872eda4693fe88fdf65f69f49423a57a8cb12
Elisha Kay <kramer1346@gmail.com>
2017-10-18 01:38:06 +0300

    added dynamic dropdown list to client view

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = '*';
+$database = 'moviehol_PHP-Login';
+
+try{
+	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch(PDOException $e){
+	die( "Connection failed: " . $e->getMessage());
+}
--- dropdown/db_connect.php
+++ dropdown/db_connect.php
@@ -3,7 +3,7 @@
 /* Database connection start */
 $servername = "localhost";
 $username = "root";
-$password = "pokemon7";
+$password = "*";
 $dbname = "phpzag_demos";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
