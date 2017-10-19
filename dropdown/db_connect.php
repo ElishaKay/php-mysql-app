d0bfadead55eba8cfd5fd36f32cc94721059a0f5
Elisha Kramer <Kramer1346@gmail.com>
2017-10-20 02:55:16 +0300

    Update db_connect.php

--- dropdown/db_connect.php
+++ dropdown/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Database connection start */
-$servername = "localhost";
-$username = "root";
-$password = "pokemon7";
-$dbname = "moviehol_PHP-Login";
+$servername = "*";
+$username = "*";
+$password = "*";
+$dbname = "*";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
